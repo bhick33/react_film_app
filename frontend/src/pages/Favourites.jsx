7d8b7614b90f891577299a3fa33258e5ca313b47
bhick33 <82394542+bhick33@users.noreply.github.com>
2025-02-03 15:07:23 +0000

    Favourites.jsx component - Add fuctionality

---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,8 @@
+function Favourites() {
+    return <div className="favourites-empty">
+        <h2>No favourite Movies yet</h2>
+        <p>Start adding movies to your favourites, they will appear here.</p>
+    </div>
+}
+
+export default Favourites
