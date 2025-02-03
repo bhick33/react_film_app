16a6b09d1035a7b6bc20f5f78f6febc102b27108
bhick33 <82394542+bhick33@users.noreply.github.com>
2025-02-03 14:30:04 +0000

    Remove movieCard from App.jsx
    Removed the import of MovieCard and useage to replace with HomePage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
+import Home from "./pages/Home"
 
 function App() {
   return (
     <>
-      <MovieCard movie={{title: "Ben's Film!", release_date: "29/12/1987"}}/>
+      <Home />
     </>
   )
 }
